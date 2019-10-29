460f8a5f3ef4c64fbcf1b0c59f3285f6ce8052a7
Chance Penner <chance.penner2016@gmail.com>
2019-10-29 14:32:44 -0500

    Add CameraMovement script

---
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothingSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,
+                                                    target.position.y,
+                                                    transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, 
+                targetPosition, 
+                    smoothingSpeed);
+        }
+    }
+}
