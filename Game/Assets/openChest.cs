a850ce73d375b95b315cac0c134bf9999208e6a4
Thomas Gardner <thomas.a.gardner22@gmail.com>
2019-10-30 20:57:20 -0500

    add chest open animation

---
+++ Game/Assets/openChest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openChest : MonoBehaviour
+{
+
+    public string tagName;
+    public GameObject[] objects;
+    private bool chestOpen;
+    private Animator anim; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        chestOpen = false;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag(tagName) && !chestOpen)
+        {
+            chestOpen = true;
+            anim.SetBool("isOpened", true);
+            Debug.Log("Hit trigger!");
+        }
+    }
+
+}
