e119d2969ba06f09207d9690592800ab1cd952d6
Thomas Gardner <thomas.a.gardner22@gmail.com>
2019-10-30 21:45:19 -0500

    add wall collisions

---
+++ Game/Assets/UpdateLayerTilemap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class UpdateLayerTilemap : MonoBehaviour
+{
+    private TilemapRenderer objectLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectLayer = GetComponent<TilemapRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.transform.position.y);
+        if(GameObject.Find("Player").transform.position.y > gameObject.transform.position.y)
+        {
+            objectLayer.sortingOrder = 1;
+        }
+        else
+        {
+            objectLayer.sortingOrder = 3;
+        }
+    }
+}
