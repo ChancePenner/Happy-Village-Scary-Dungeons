2ab9993cb0cb328fff41e0354a8c414d53ba3395
Thomas Gardner <thomas.a.gardner22@gmail.com>
2019-11-01 21:17:53 -0500

    add open door after enemy slaying

---
+++ Game/Assets/checkDoor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkDoor : MonoBehaviour
+{
+
+    private int numEnemies;
+    private GameObject[] enemies;
+    private bool enemySpawned;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        numEnemies = 0;
+        enemySpawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy1");
+        
+        if (enemies.Length > 0)
+        {
+            enemySpawned = true;
+        }
+
+        if (enemySpawned)
+        {
+            if (enemies.Length == 0)        //at this point all enemies are dead
+            {
+                animator.SetBool("allEnemiesDead",true);
+            }
+        }
+    }
+    
+}
