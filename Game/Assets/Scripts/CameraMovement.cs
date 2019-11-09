c08804ba0af02c06adb5417b3922057e95551abf
Thomas Gardner <thomas.a.gardner22@gmail.com>
2019-11-08 21:21:19 -0600

    add camera boundary
    Co-Authored-By: Chance Penner <chancepenner@users.noreply.github.com>

--- Game/Assets/Scripts/CameraMovement.cs
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -11,14 +11,18 @@ TeamName: BigSegFaultEnergy
   * \Brief:  This script in charge of main camera auto following character
  **/
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public float smoothingSpeed;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
 
     /*
      * @ pre none
@@ -33,6 +37,13 @@ public class CameraMovement : MonoBehaviour
             Vector3 targetPosition = new Vector3(target.position.x,
                 target.position.y,
                 transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x,
+                                                minPosition.x,
+                                                maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y,
+                                                minPosition.y,
+                                                maxPosition.y);
             transform.position = Vector3.Lerp(transform.position,
                 targetPosition,
                 smoothingSpeed);
