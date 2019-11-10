6c35800aa7ac17ea0ef30654308d6678914ec5f2
Thomas Gardner <thomas.a.gardner22@gmail.com>
2019-11-10 17:44:15 -0600

    add boxes and swtiches to room 1
    Co-Authored-By: Chance Penner <chancepenner@users.noreply.github.com>

---
+++ Game/Assets/Scripts/buttonPress.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonPress : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (gameObject.CompareTag("blueButton"))
+        {
+            if(other.gameObject.CompareTag("blueBox"))
+            {
+                Debug.Log("Blue button");
+            }
+        }
+        else if (gameObject.CompareTag("redButton"))
+        {
+            if(other.gameObject.CompareTag("redBox"))
+            {
+                Debug.Log("Red button");
+            }
+        }
+        
+    }
+    
+}
