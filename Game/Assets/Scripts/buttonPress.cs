15a30cedae254bf207fb4af3a781170cc2c272d1
Chance Penner <chance.penner2016@gmail.com>
2019-11-11 11:47:16 -0600

    Add button down animations and trigger
    Co-Authored-By: sarah-scott <sarah-scott@users.noreply.github.com>

--- Game/Assets/Scripts/buttonPress.cs
+++ Game/Assets/Scripts/buttonPress.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class buttonPress : MonoBehaviour
 {
-    
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -24,17 +26,36 @@ public class buttonPress : MonoBehaviour
         {
             if(other.gameObject.CompareTag("blueBox"))
             {
-                Debug.Log("Blue button");
+                anim.SetBool("isBlueButtonDown", true);  
             }
         }
         else if (gameObject.CompareTag("redButton"))
         {
             if(other.gameObject.CompareTag("redBox"))
             {
-                Debug.Log("Red button");
+               anim.SetBool("isRedButtonDown", true);  
+            }
+           
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (gameObject.CompareTag("blueButton"))
+        {
+            if(other.gameObject.CompareTag("blueBox"))
+            {
+                anim.SetBool("isBlueButtonDown", false);
             }
         }
-        
+        else if (gameObject.CompareTag("redButton"))
+        {
+            if(other.gameObject.CompareTag("redBox"))
+            {
+                anim.SetBool("isRedButtonDown", false);  
+            }
+           
+        }
     }
-    
 }
