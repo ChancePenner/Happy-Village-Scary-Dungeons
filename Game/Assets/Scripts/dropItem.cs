7c6dca9ae0366ecd6953bdc7104db527e6c29b3b
Thomas Gardner <thomas.a.gardner22@gmail.com>
2019-11-20 20:28:51 -0600

    add scene transitions for the different scenes

---
+++ Game/Assets/Scripts/dropItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropItem : MonoBehaviour
+{
+    public SpriteRenderer item;
+
+    void itemDrop()
+    {
+        Instantiate(item, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+}
--- Game/Assets/Scripts/mainMenu.cs
+++ Game/Assets/Scripts/mainMenu.cs
@@ -8,7 +8,7 @@ public class mainMenu : MonoBehaviour
   public void startGame()
   {
 //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   //will use for loading town for real game
-    SceneManager.LoadScene("Dungeon1");
+    SceneManager.LoadScene("Town");
   }
 
   public void exitGame()
