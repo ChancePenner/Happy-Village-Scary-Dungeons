cfc955fa76d1d6da6e5feb3705934f4ec494b5ed
Chance Penner <chance.penner2016@gmail.com>
2019-11-27 15:34:59 -0600

    Add portal to room 3
    Bug where portal not showing up in game. seems to be a layering issue

---
+++ Game/Assets/Scripts/portal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    public objectVector teleportLocation;
+
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other != null)
+        {
+            other.gameObject.GetComponent<Transform>().position = teleportLocation.initial;    //teleport player to next room
+        }
+    }
+}
