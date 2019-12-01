8aec87088d70c737427afa768fc1f5d34295ce81
Haonan Hu <hhn97@ku.edu>
2019-12-01 11:49:36 -0600

    Added several tests

--- Game/Assets/Scripts/test.cs
+++ Game/Assets/Scripts/test.cs
@@ -15,6 +15,9 @@ public class test : MonoBehaviour
         test3();
         test4();
         test5();
+        test6();
+        test7();
+        test8();
     }
 
     private void test1() //test player does not have sword by default
@@ -102,6 +105,62 @@ public class test : MonoBehaviour
                 Debug.Log("Test player health is static: FAILED\n");
             }
             
+        }
+        Destroy(Player);
+    }
+    
+    private void test6() //test player after taking damage
+    {
+        Instantiate(Player);
+        if (Player != null)
+        {
+            Player.GetComponent<playerHealth>().harmPlayer(2);
+            if (Player.GetComponent<playerHealth>().getHealth() == 1)
+            {
+                Debug.Log(("Test player after taking damage: PASS\n"));
+            }
+            else
+            {
+                Debug.Log("Test player after taking damage: FAILED\n");
+            }
+        }
+        Destroy(Player);
+    }
+    
+    private void test7() //test player give health
+    {
+        Instantiate(Player);
+        if (Player != null)
+        {
+            Player.GetComponent<playerHealth>().giveHealth(1);
+            if (Player.GetComponent<playerHealth>().getHealth() == 2)
+            {
+                Debug.Log(("Test player give health: PASS\n"));
+            }
+            else
+            {
+                Debug.Log("Test player give health: FAILED\n");
+            }
+        }
+        Destroy(Player);
+    }
+    
+    private void test8() //test player after taking too much damage(Larger than your maximum amount)
+    {
+        Instantiate(Player);
+        if (Player != null)
+        {
+            Debug.Log(Player.GetComponent<playerHealth>().getHealth());
+            Player.GetComponent<playerHealth>().harmPlayer(3);
+            Debug.Log(Player.GetComponent<playerHealth>().getHealth());
+            if (Player.GetComponent<playerHealth>().getHealth() == 0)
+            {
+                Debug.Log(("Test player after taking too much damage: PASS\n"));
+            }
+            else
+            {
+                Debug.Log("Test player after taking too much damage: FAILED\n");
+            }
         }
         Destroy(Player);
     }
